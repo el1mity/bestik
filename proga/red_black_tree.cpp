d16d43844dd4ea392ba724d7cf136f87532fdfcb
el1mity <bogatk1naa@yandex.ru>
2026-05-15 20:05:57 +0400

    update(proga): заменить red_black_tree.cpp на новую версию

--- proga/red_black_tree.cpp
+++ proga/red_black_tree.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
 
-
 enum Color { RED, BLACK };
 
 struct tree {
@@ -12,274 +11,296 @@ struct tree {
     tree* parent;
 };
 
-tree* node(int x) {
+tree* node(tree* p, int X) {
     tree* n = new tree;
-    n->inf = x;
-    n->color = RED;  // Новый узел всегда красный
+    n->inf = X;
+    n->color = RED;
+    n->left = n->right = NULL;
+    n->parent = p;
+    return n;
+}
+
+tree* root(int X) {
+    tree* n = new tree;
+    n->inf = X;
+    n->color = BLACK;
     n->left = n->right = NULL;
     n->parent = NULL;
     return n;
 }
 
-void rotate_left(tree*& root, tree* x) {
-    tree* y = x->right;
-    x->right = y->left;
-    if (y->left != NULL)
-        y->left->parent = x;
-    y->parent = x->parent;
-    if (x->parent == NULL)
-        root = y;
-    else if (x == x->parent->left)
-        x->parent->left = y;
-    else
-        x->parent->right = y;
-    y->left = x;
-    x->parent = y;
+tree* grandparent(tree* X) {
+    if (X != NULL && X->parent != NULL)
+        return X->parent->parent;
+    return NULL;
 }
 
-void rotate_right(tree*& root, tree* x) {
-    tree* y = x->left;
-    x->left = y->right;
-    if (y->right != NULL)
-        y->right->parent = x;
-    y->parent = x->parent;
-    if (x->parent == NULL)
-        root = y;
-    else if (x == x->parent->right)
-        x->parent->right = y;
-    else
-        x->parent->left = y;
-    y->right = x;
-    x->parent = y;
+tree* uncle(tree* X) {
+    tree* g = grandparent(X);
+    if (g == NULL) return NULL;
+    if (X->parent == g->left) return g->right;
+    else return g->left;
 }
 
-void fix_insert(tree*& root, tree* z) {
-    while (z != root && z->parent != NULL && z->parent->color == RED) {
-        if (z->parent == z->parent->parent->left) {
-            tree* y = z->parent->parent->right; // дядя
-
-            if (y != NULL && y->color == RED) {
-                // случай 1: дядя красный
-                z->parent->color = BLACK;
-                y->color = BLACK;
-                z->parent->parent->color = RED;
-                z = z->parent->parent;
-            }
-            else {
-                // случай 2: дядя черный
-                if (z == z->parent->right) {
-                    z = z->parent;
-                    rotate_left(root, z);
-                }
-                // случай 3
-                z->parent->color = BLACK;
-                z->parent->parent->color = RED;
-                rotate_right(root, z->parent->parent);
-            }
-        }
-        else {
-            // симметричный случай для правого родителя
-            tree* y = z->parent->parent->left;
-
-            if (y != NULL && y->color == RED) {
-                z->parent->color = BLACK;
-                y->color = BLACK;
-                z->parent->parent->color = RED;
-                z = z->parent->parent;
-            }
-            else {
-                if (z == z->parent->left) {
-                    z = z->parent;
-                    rotate_right(root, z);
-                }
-                z->parent->color = BLACK;
-                z->parent->parent->color = RED;
-                rotate_left(root, z->parent->parent);
-            }
-        }
+tree* sibling(tree* X) {
+    if (X != NULL && X->parent != NULL) {
+        if (X == X->parent->left) return X->parent->right;
+        else return X->parent->left;
     }
-    root->color = BLACK;
+    return NULL;
 }
 
-void insert(tree*& tr, int x) {
-    tree* n = node(x);
-    if (!tr) {
-        tr = n;
-        tr->color = BLACK; // корень всегда черный
-        return;
-    }
-    // обычная вставка в бинарное дерево поиска
-    tree* y = NULL;
-    tree* current = tr;
-    while (current != NULL) {
-        y = current;
-        if (n->inf < current->inf)
-            current = current->left;
-        else if (n->inf > current->inf)
-            current = current->right;
-        else {
-            delete n;
-            return;
-        }
-    }
-    n->parent = y;
-    if (n->inf < y->inf)
-        y->left = n;
-    else
-        y->right = n;
-
-    // восстанавливаем свойства красно-черного дерева
-    fix_insert(tr, n);
+void rotate_left(tree*& tr, tree* X) {
+    tree* Y = X->right;
+    X->right = Y->left;
+    if (Y->left != NULL) Y->left->parent = X;
+    Y->parent = X->parent;
+    if (X->parent == NULL) tr = Y;
+    else if (X == X->parent->left) X->parent->left = Y;
+    else X->parent->right = Y;
+    Y->left = X;
+    X->parent = Y;
 }
 
-tree* find_min(tree* tr) {
-    while (tr->left != NULL)
-        tr = tr->left;
-    return tr;
+void rotate_right(tree*& tr, tree* X) {
+    tree* Y = X->left;
+    X->left = Y->right;
+    if (Y->right != NULL) Y->right->parent = X;
+    Y->parent = X->parent;
+    if (X->parent == NULL) tr = Y;
+    else if (X == X->parent->right) X->parent->right = Y;
+    else X->parent->left = Y;
+    Y->right = X;
+    X->parent = Y;
 }
 
-tree* find_max(tree* tr) {
-    while (tr->right != NULL)
-        tr = tr->right;
-    return tr;
+void insert_case1(tree*& tr, tree* X);
+void insert_case2(tree*& tr, tree* X);
+void insert_case3(tree*& tr, tree* X);
+void insert_case4(tree*& tr, tree* X);
+void insert_case5(tree*& tr, tree* X);
+
+void insert_case1(tree*& tr, tree* X) {
+    if (X->parent == NULL) X->color = BLACK;
+    else insert_case2(tr, X);
 }
 
-// замена одного узла другим
-void transplant(tree*& root, tree* u, tree* v) {
-    if (u->parent == NULL)
-        root = v;
-    else if (u == u->parent->left)
-        u->parent->left = v;
-    else
-        u->parent->right = v;
-    if (v != NULL)
-        v->parent = u->parent;
+void insert_case2(tree*& tr, tree* X) {
+    if (X->parent->color == RED) insert_case3(tr, X);
 }
 
-// восстановление свойств после удаления
-void fix_delete(tree*& root, tree* x) {
-    while (x != root && (x == NULL || x->color == BLACK)) {
-        if (x == x->parent->left) {
-            tree* w = x->parent->right; // брат
+void insert_case3(tree*& tr, tree* X) {
+    tree* U = uncle(X);
+    tree* G = grandparent(X);
 
-            if (w != NULL && w->color == RED) {
-                w->color = BLACK;
-                x->parent->color = RED;
-                rotate_left(root, x->parent);
-                w = x->parent->right;
-            }
-
-            if ((w == NULL || w->left == NULL || w->left->color == BLACK) &&
-                (w == NULL || w->right == NULL || w->right->color == BLACK)) {
-                if (w != NULL) w->color = RED;
-                x = x->parent;
-            }
-            else {
-                if (w != NULL && (w->right == NULL || w->right->color == BLACK)) {
-                    if (w->left != NULL) w->left->color = BLACK;
-                    if (w != NULL) w->color = RED;
-                    rotate_right(root, w);
-                    w = x->parent->right;
-                }
-
-                if (w != NULL) {
-                    w->color = x->parent->color;
-                    x->parent->color = BLACK;
-                    if (w->right != NULL) w->right->color = BLACK;
-                    rotate_left(root, x->parent);
-                }
-                x = root;
-            }
-        }
-        else {
-            tree* w = x->parent->left;   // симметричный случай
-
-            if (w != NULL && w->color == RED) {
-                w->color = BLACK;
-                x->parent->color = RED;
-                rotate_right(root, x->parent);
-                w = x->parent->left;
-            }
-
-            if ((w == NULL || w->right == NULL || w->right->color == BLACK) &&
-                (w == NULL || w->left == NULL || w->left->color == BLACK)) {
-                if (w != NULL) w->color = RED;
-                x = x->parent;
-            }
-            else {
-                if (w != NULL && (w->left == NULL || w->left->color == BLACK)) {
-                    if (w->right != NULL) w->right->color = BLACK;
-                    if (w != NULL) w->color = RED;
-                    rotate_left(root, w);
-                    w = x->parent->left;
-                }
-
-                if (w != NULL) {
-                    w->color = x->parent->color;
-                    x->parent->color = BLACK;
-                    if (w->left != NULL) w->left->color = BLACK;
-                    rotate_right(root, x->parent);
-                }
-                x = root;
-            }
-        }
-    }
-    if (x != NULL)
-        x->color = BLACK;
-}
-
-void delete_node(tree*& root, int value) {
-    tree* z = root;
-    while (z != NULL && z->inf != value) {
-        if (value < z->inf)
-            z = z->left;
-        else
-            z = z->right;
-    }
-    if (z == NULL) {
-        cout << "Элемент не найден!" << endl;
-        return;
-    }
-    tree* y = z;
-    tree* x = NULL;
-    Color y_color = y->color;
-
-    if (z->left == NULL) {
-        x = z->right;
-        transplant(root, z, z->right);
-    }
-    else if (z->right == NULL) {
-        x = z->left;
-        transplant(root, z, z->left);
+    if (U != NULL && U->color == RED && X->parent->color == RED) {
+        X->parent->color = BLACK;
+        U->color = BLACK;
+        G->color = RED;
+        insert_case1(tr, G);
     }
     else {
-        y = find_min(z->right);
-        y_color = y->color;
-        x = y->right;
-        if (y->parent == z) {
-            if (x != NULL) x->parent = y;
-        }
-        else {
-            transplant(root, y, y->right);
-            y->right = z->right;
-            if (y->right != NULL) y->right->parent = y;
-        }
-
-        transplant(root, z, y);
-        y->left = z->left;
-        if (y->left != NULL) y->left->parent = y;
-        y->color = z->color;
-    }
-    delete z;
-
-    if (y_color == BLACK) {
-        fix_delete(root, x);
+        insert_case4(tr, X);
     }
 }
 
+void insert_case4(tree*& tr, tree* X) {
+    tree* G = grandparent(X);
+
+    if (X == X->parent->right && X->parent == G->left) {
+        rotate_left(tr, X);
+        X = X->left;
+    }
+    else if (X == X->parent->left && X->parent == G->right) {
+        rotate_right(tr, X);
+        X = X->right;
+    }
+    insert_case5(tr, X);
+}
+
+void insert_case5(tree*& tr, tree* X) {
+    tree* G = grandparent(X);
+    X->parent->color = BLACK;
+    G->color = RED;
+    if (X == X->parent->left && X->parent == G->left) rotate_right(tr, G);
+    else rotate_left(tr, G);
+}
+
+void insert(tree*& tr, tree* Prev, int X) {
+    if (X < Prev->inf && Prev->left == NULL) {
+        Prev->left = node(Prev, X);
+        insert_case1(tr, Prev->left);
+    }
+    else if (X > Prev->inf && Prev->right == NULL) {
+        Prev->right = node(Prev, X);
+        insert_case1(tr, Prev->right);
+    }
+    else if (X < Prev->inf && Prev->left != NULL) {
+        insert(tr, Prev->left, X);
+    }
+    else if (X > Prev->inf && Prev->right != NULL) {
+        insert(tr, Prev->right, X);
+    }
+}
+
+void delete_case1(tree*& tr, tree* X);
+void delete_case2(tree*& tr, tree* X);
+void delete_case3(tree*& tr, tree* X);
+void delete_case4(tree*& tr, tree* X);
+void delete_case5(tree*& tr, tree* X);
+void delete_case6(tree*& tr, tree* X);
+
+void replace(tree*& tr, tree* X) {
+    tree* ch = NULL;
+    if (X->left != NULL) {
+        ch = X->left;
+        ch->parent = X->parent;
+        if (X->parent != NULL) {
+            if (X == X->parent->left) X->parent->left = ch;
+            else X->parent->right = ch;
+        }
+        else tr = ch;
+    }
+    else if (X->right != NULL) {
+        ch = X->right;
+        ch->parent = X->parent;
+        if (X->parent != NULL) {
+            if (X == X->parent->left) X->parent->left = ch;
+            else X->parent->right = ch;
+        }
+        else tr = ch;
+    }
+}
+
+void delete_case1(tree*& tr, tree* X) {
+    if (X->parent == NULL) {
+        if (X->left != NULL) tr = X->left;
+        else tr = X->right;
+    }
+    else delete_case2(tr, X);
+}
+
+void delete_case2(tree*& tr, tree* X) {
+    tree* S = sibling(X);
+    if (S != NULL && S->color == RED) {
+        X->parent->color = RED;
+        S->color = BLACK;
+        if (X == X->parent->left) rotate_left(tr, X->parent);
+        else rotate_right(tr, X->parent);
+    }
+    delete_case3(tr, X);
+}
+
+void delete_case3(tree*& tr, tree* X) {
+    tree* S = sibling(X);
+    if (X->parent->color == BLACK && S != NULL && S->color == BLACK &&
+        (S->left == NULL || S->left->color == BLACK) &&
+        (S->right == NULL || S->right->color == BLACK)) {
+        S->color = RED;
+        delete_case1(tr, X->parent);
+    }
+    else delete_case4(tr, X);
+}
+
+void delete_case4(tree*& tr, tree* X) {
+    tree* S = sibling(X);
+    if (X->parent->color == RED && S != NULL && S->color == BLACK &&
+        (S->left == NULL || S->left->color == BLACK) &&
+        (S->right == NULL || S->right->color == BLACK)) {
+        S->color = RED;
+        X->parent->color = BLACK;
+    }
+    else delete_case5(tr, X);
+}
+
+void delete_case5(tree*& tr, tree* X) {
+    tree* S = sibling(X);
+    if (S->color == BLACK) {
+        if (X == X->parent->left && S->left != NULL && S->left->color == RED &&
+            (S->right == NULL || S->right->color == BLACK)) {
+            S->color = RED;
+            S->left->color = BLACK;
+            rotate_right(tr, S);
+        }
+        else if (X == X->parent->right && S->right != NULL && S->right->color == RED &&
+            (S->left == NULL || S->left->color == BLACK)) {
+            S->color = RED;
+            S->right->color = BLACK;
+            rotate_left(tr, S);
+        }
+    }
+    delete_case6(tr, X);
+}
+
+void delete_case6(tree*& tr, tree* X) {
+    tree* S = sibling(X);
+    S->color = X->parent->color;
+    X->parent->color = BLACK;
+    if (X == X->parent->left) {
+        if (S->right != NULL) S->right->color = BLACK;
+        rotate_left(tr, X->parent);
+    }
+    else {
+        if (S->left != NULL) S->left->color = BLACK;
+        rotate_right(tr, X->parent);
+    }
+}
+
+tree* find_node(tree* tr, int value) {
+    while (tr != NULL && tr->inf != value) {
+        if (value < tr->inf) tr = tr->left;
+        else tr = tr->right;
+    }
+    return tr;
+}
+
+void delete_one(tree*& tr, tree* X) {
+    if (X->left != NULL && X->right != NULL) {
+        tree* buf = X->right;
+        while (buf->left != NULL) buf = buf->left;
+        X->inf = buf->inf;
+        X = buf;
+    }
+
+    if (X->left != NULL || X->right != NULL) {
+        tree* ch = (X->left != NULL) ? X->left : X->right;
+        replace(tr, X);
+        if (X->color == BLACK) {
+            if (ch->color == RED) ch->color = BLACK;
+            else delete_case1(tr, ch);
+        }
+    }
+    else {
+        if (X->color == BLACK) delete_case1(tr, X);
+        if (X->parent != NULL) {
+            if (X == X->parent->left) X->parent->left = NULL;
+            else X->parent->right = NULL;
+        }
+    }
+    delete X;
+}
+
+void delete_value(tree*& tr, int value) {
+    tree* X = find_node(tr, value);
+    if (X == NULL) {
+        cout << "Элемент " << value << " не найден!" << endl;
+        return;
+    }
+    delete_one(tr, X);
+}
+
+// Симметричный обход
+void inorder(tree* tr) {
+    if (tr == NULL) return;
+    inorder(tr->left);
+    cout << tr->inf << " ";
+    inorder(tr->right);
+}
+
+// Вывод всех листьев
 void print_leaves(tree* tr) {
-    if (!tr) return;
-    if (!tr->left && !tr->right) {
+    if (tr == NULL) return;
+    if (tr->left == NULL && tr->right == NULL) {
         cout << tr->inf << " ";
         return;
     }
@@ -287,17 +308,9 @@ void print_leaves(tree* tr) {
     print_leaves(tr->right);
 }
 
-// симметричный обход
-void inorder(tree* tr) {
-    if (!tr) return;
-    inorder(tr->left);
-    cout << tr->inf << " ";
-    inorder(tr->right);
-}
-
-// очистка дерева
+// Очистка дерева
 void clear_tree(tree*& tr) {
-    if (!tr) return;
+    if (tr == NULL) return;
     clear_tree(tr->left);
     clear_tree(tr->right);
     delete tr;
@@ -309,14 +322,28 @@ int main() {
     tree* tr = NULL;
     int n, val;
     cout << "Введите количество элементов: "; cin >> n;
-    cout << "Введите " << n << " целых чисел: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> val;
-        insert(tr, val);
+
+    if (n > 0) {
+        cout << "Введите " << n << " элементов: ";
+        for (int i = 0; i < n; ++i) {
+            cin >> val;
+            if (i == 0) tr = root(val);
+            else {
+                insert(tr, tr, val);
+            }
+        }
     }
-    cout << "Симметричный обход: ";
+    else {
+        cout << "Дерево пустое!";
+        return 0;
+    }
+
+    cout << "\nСимметричный обход: ";
     inorder(tr);
+
     cout << "\nЛистья дерева: ";
     print_leaves(tr);
+
+    clear_tree(tr);
     return 0;
 }
